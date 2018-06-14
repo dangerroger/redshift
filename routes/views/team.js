aee32e00b8e467c59a8d5d149d264469b810e7b6
iamrahul <drseewal@gmail.com>
2018-06-14 20:07:33 +0100

    photos added

--- routes/views/team.js
+++ routes/views/team.js
@@ -3,20 +3,11 @@ exports = module.exports = function (req, res) {
 var view = new keystone.View(req, res);
     var locals = res.locals;
 
-//set locals
+    //set locals
+    locals.section = 'team';
+    // load Team
+    view.query('teams',keystone.list('Team').model.find());
+    //Render team page
+    view.render('team');
 
-locals.section = 'team';
-// load Team
-
-view.query('teams',keystone.list('Team').model.find(
-
-));
-
-
-
-
-
-//Render team page
-
-view.render('team');
-};
+                                                };
